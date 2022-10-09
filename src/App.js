886e3f79f22ffe2ba48b206140528bc2bdbbc9e3
Nolan Truong <duyth1203@gmail.com>
2022-10-09 08:09:31 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import * as React from "react"
+import useSyncQueryParams from "use-sync-query-params"
+
+const Thing = () => {
+  const params = useSyncQueryParams({ foo: "bar" })
+
+  console.log("render")
+
+  return (
+    <div>
+      <p>{params.getParam("foo")}</p>
+      <p>{window.location.search}</p>
+      <pre>{JSON.stringify(params.getAllParams())}</pre>
+      <br />
+      <button onClick={() => params.setParam("foo", "baz")}>
+        Change bar to baz
+      </button>
+      <br />
+      <button onClick={() => params.setParam("foo", "bar")}>
+        Change baz to bar
+      </button>
+      <br />
+      <button onClick={() => params.clearParam("foo")}>
+        Clear query param
+      </button>
+      <br />
+      <button onClick={() => params.setParam("foo", null)}>
+        Clear query param null
+      </button>
+      <br />
+      <button onClick={() => params.setParam("foo", undefined)}>
+        Clear query param undefined
+      </button>
+      <br />
+      <button onClick={() => params.setParam("foo", "")}>
+        Clear query param empty string
+      </button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Thing />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
